96300bb631ebf696df64d7f57cbfdd6ce3946640
Tiago Silva <1140437@isep.ipp.pt>
2017-12-12 15:01:11 +0000

    Gradient color for Channel VC.
    Gradient color for Channel VC. Also changed the top view in the Chat VC to have the same gradient color.

---
+++ Smack/Views/GradientView.swift
@@ -0,0 +1,34 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Tiago Santos on 12/12/17.
+//  Copyright © 2017 Tiago Santos. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds // The frame it belongs to.
+        self.layer.insertSublayer(gradientLayer, at: 0) // 0 -> First layer.
+    }
+}
