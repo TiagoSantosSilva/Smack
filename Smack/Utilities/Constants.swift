875c10f0180e747bf6220363bcc389c65d6d9381
Tiago Silva <1140437@isep.ipp.pt>
2017-12-12 15:28:50 +0000

    Login VC creation.
    Login VC creation.

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Tiago Santos on 12/12/17.
+//  Copyright © 2017 Tiago Santos. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let ToLogin = "toLogin"
+
