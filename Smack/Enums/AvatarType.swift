60a42b4333f67fe3a7c1631b7c292bc3629b69e8
Tiago Silva <1140437@isep.ipp.pt>
2017-12-14 22:38:49 +0000

    Avatar type enums
    Avatar type enums: Dark and Light

---
+++ Smack/Enums/AvatarType.swift
@@ -0,0 +1,14 @@
+//
+//  AvatarType.swift
+//  Smack
+//
+//  Created by Tiago Santos on 14/12/17.
+//  Copyright © 2017 Tiago Santos. All rights reserved.
+//
+
+import Foundation
+
+enum AvatarType {
+    case dark
+    case light
+}
